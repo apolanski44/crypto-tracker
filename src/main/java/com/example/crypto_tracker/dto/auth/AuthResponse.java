4b58e6e43b228123b9c19f708b22edc0f1ac0e3e
Adrian <apolanski1234@gmail.com>
2026-03-05 20:23:07 +0100

    Add dtos

---
+++ src/main/java/com/example/crypto_tracker/dto/auth/AuthResponse.java
@@ -0,0 +1,3 @@
+package com.example.crypto_tracker.dto.auth;
+
+public record AuthResponse(String accessToken) {}
--- src/main/java/com/example/crypto_tracker/dto/user/CreateUserDTO.java
+++ src/main/java/com/example/crypto_tracker/dto/user/CreateUserDTO.java
@@ -3,21 +3,22 @@ package com.example.crypto_tracker.dto.user;
 import com.example.crypto_tracker.enums.UserRole;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateUserDTO {
-    @NotNull
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
-    @NotNull
+    @NotBlank(message = "Password is required")
     //TODO: Add password validator
     private String password;
     @Nullable
     private String firstName;
     @Nullable
     private String lastName;
-    @NotNull
+    @NotBlank
     private UserRole role;
 }
