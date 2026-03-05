0974dad0ab6135fffe8ac5e504af217c79bbb36c
Adrian <apolanski1234@gmail.com>
2026-03-05 20:20:16 +0100

    Add auth services

---
+++ src/main/java/com/example/crypto_tracker/service/auth/RegistrationService.java
@@ -0,0 +1,42 @@
+package com.example.crypto_tracker.service.auth;
+
+import com.example.crypto_tracker.dto.auth.RegistrationDTO;
+import com.example.crypto_tracker.exception.UserAlreadyVerifiedException;
+import com.example.crypto_tracker.model.User;
+import com.example.crypto_tracker.model.VerificationToken;
+import com.example.crypto_tracker.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserService userService;
+    private final VerificationTokenService tokenService;
+    private final AuthMailer authMailer;
+
+    @Transactional
+    public void register(RegistrationDTO dto) {
+        userService.registerUser(dto)
+                .ifPresent(user -> {
+                    String token = tokenService.createToken(user);
+                    authMailer.sendActivationEmail(user.getEmail(), token);
+                });
+    }
+
+    @Transactional
+    public void confirmEmail(String code) {
+        VerificationToken token = tokenService.getValidToken(code);
+
+        User user = token.getUser();
+
+        if (user.isAccountVerified()) {
+            throw new UserAlreadyVerifiedException("User has been already verified.");
+        }
+
+        user.setAccountVerified(true);
+
+        tokenService.deleteToken(token);
+    }
+}
