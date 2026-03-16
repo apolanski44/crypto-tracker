aad4f3ffa182c153538a3bb796204130e01d6bf1
Adrian <apolanski1234@gmail.com>
2026-03-16 20:53:17 +0100

    Remvoe redundant transactional addnotation

--- src/main/java/com/example/crypto_tracker/service/auth/AuthMailer.java
+++ src/main/java/com/example/crypto_tracker/service/auth/AuthMailer.java
@@ -3,14 +3,12 @@ package com.example.crypto_tracker.service.auth;
 import com.example.crypto_tracker.service.email.EmailSenderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthMailer {
     private final EmailSenderService emailSenderService;
 
-    @Transactional
     public void sendActivationEmail(String email, String token) {
         String message = "Your activation code: " + token;
 
